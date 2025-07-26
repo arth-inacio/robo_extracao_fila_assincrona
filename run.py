c17989f4ef83293073677edcc2d2df9bccf699c4
Arthur <arthur.inacio3@gmail.com>
2025-07-26 13:47:00 -0300

    feat[servimed]: Recriando coleta dos produtos por playwright-async

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import asyncio
 from servimed_scraper import ServimedScraper
 
 async def main():
-    coletor = ServimedScraper("usuario", "senha")
+    coletor = ServimedScraper("juliano@farmaprevonline.com.br", "a007299A")
+    await coletor.playwright_start()
     await coletor._coletor_cadastros()
-
+    await coletor.playwright_finish()
 if __name__ == "__main__":
     asyncio.run(main())
--- servimed_scraper.py
+++ servimed_scraper.py
@@ -1,5 +1,7 @@
-import asyncio
-from utils.httpx._async import AsyncHttpx
+import re
+import json
+from bs4 import BeautifulSoup
+from playwright.async_api import async_playwright, TimeoutError
 
 class ServimedScraper:
     usuario: str
@@ -9,16 +11,62 @@ class ServimedScraper:
     def __init__(self, usuario: str, senha: str) -> None:
         self.usuario = usuario
         self.senha = senha
+        self.playwright = None
+        self.browser = None
+        self.context = None
+        self.page = None
 
-        self.session = AsyncHttpx("Acesso Principal").session
-        self.session.base_url = self.url
+    async def playwright_start(self) -> None:
+        # Método que Inicializa o playwright
+        self.playwright = await async_playwright().start()
+        self.browser = await self.playwright.chromium.launch(headless=False)
+        self.context = await self.browser.new_context()
+        self.page = await self.context.new_page()
+        self.page.set_default_timeout(40000)
+
+    async def playwright_finish(self) -> None:
+        # Finaliza a sessão do playwright
+        await self.context.close()
+        await self.playwright.stop()
+        await self.browser.close()
 
     async def _coletor_cadastros(self):
-        await self.session.get(self.url)
-        payload = {
-            "senha": self.senha,
-            "usuario": self.usuario,
-        }
-        response = await self.session.post("api/usuario/login", json=payload)
-        print(response.json())
+        await self.page.goto(f"{self.url}login")
+        await self.page.wait_for_load_state("networkidle")
+        # await self.page.wait_for_selector("input[name=\"username\"]")
+
+        await self.page.type("input[name=\"username\"]", self.usuario)
+        await self.page.type("input[name=\"password\"]", self.senha)
+
+        # Clica no botao de login
+        await self.page.locator("button[class=\"btn btn-block btn-success\"]").click()
+        await self.page.wait_for_selector("button[class=\"swal2-cancel swal2-styled\"]")
+
+        # Cancela o alerta de atualização dos dados
+        await self.page.locator("button[class=\"swal2-cancel swal2-styled\"]").click()
+        await self.page.wait_for_load_state("networkidle")
+
+        # Clica em pedidos -> novo pedido
+        await self.page.locator("a").filter(has_text="Pedidos").click()
+        await self.page.wait_for_load_state("networkidle")
+        await self.page.get_by_role("link", name="Novo pedido").click()
+
+        await self.page.get_by_role("link", name="refresh Alterar").click()
+        await self.page.wait_for_load_state("networkidle")
+
+        # Seleciona cliente com lista de produtos
+        await self.page.get_by_text("267511").click()
+        await self.page.wait_for_load_state("networkidle")
+
+        # Preenche o campo de pesquisa de produtos
+        await self.page.locator("input[role=\"combobox\"]").fill("PARACETAMOL")
+
+        # Clica no botao pesquisar e procura a requisição com os produtos
+        async with self.page.expect_response(re.compile(r"carrinho/oculto")) as response_info:
+            await self.page.click("//html/body/app-root/pedido/div[4]/div/div[1]/div/div/div[3]/button/i")
+        response = await response_info.value
+        body = await response.body()
+        print(json.loads(body))
+      
+
         return
